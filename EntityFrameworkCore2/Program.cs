073b9baab3d75e6892d03c10e8dc07bdc55c9a27
Karen Payne <paynekaren@comcast.net>
2022-12-31 15:22:10 -0800

    added new code sample

---
+++ EntityFrameworkCore2/Program.cs
@@ -0,0 +1,53 @@
+﻿using EntityFrameworkCore2.Data;
+
+namespace EntityFrameworkCore2;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+
+        var table = CreateTableEntityFramework();
+
+        using (var context = new Context())
+        {
+            var apps = context.ApplicationSettings.ToList();
+            foreach (var app in apps)
+            {
+                table.AddRow(
+                    app.Id.ToString(), 
+                    app.AppName, 
+                    app.TheVersion, 
+                    app.VersionMajor.ToString(), 
+                    app.VersionMinor.ToString(), 
+                    app.VersionBuild.ToString(), 
+                    app.VersionRevison.ToString());
+            }
+
+            AnsiConsole.Write(table);
+
+            /*
+             * Given that TheVersion property can be used to create a Version object let's do it.
+             */
+            var firstApp = apps.FirstOrDefault();
+            Version version = new Version(firstApp!.TheVersion);
+            AnsiConsole.MarkupLine($"[cyan]{firstApp.Id,-3}{firstApp.AppName,-10}[/][yellow]{version}[/]");
+
+        }
+
+        Console.ReadLine();
+
+    }
+    public static Table CreateTableEntityFramework()
+        => new Table().RoundedBorder()
+            .AddColumn("[cyan]Id[/]")
+            .AddColumn("[cyan]Name[/]")
+            .AddColumn("[cyan]Version[/]")
+            .AddColumn("[cyan]Major[/]")
+            .AddColumn("[cyan]Minor[/]")
+            .AddColumn("[cyan]Build[/]")
+            .AddColumn("[cyan]Revison[/]")
+            .Alignment(Justify.Left)
+            .BorderColor(Color.LightSlateGrey)
+            .Title("[LightGreen]Application versions[/]");
+}
---
+++ EntityFrameworkCore2/readme.md
@@ -0,0 +1,15 @@
+﻿# About
+
+This code sample shows storing version information in a SQL-Server database table.
+
+Since each part of the version are stored as int you can take those column values and create a [Version](https://learn.microsoft.com/en-us/dotnet/api/system.version?view=net-7.0) object or use `TheVersion` column for display purposes.
+
+![Figure2](assets/figure2.png)
+
+---
+
+![Figure1](assets/Figure1.png)
+
+---
+
+![Figure3](assets/figure3.png)
